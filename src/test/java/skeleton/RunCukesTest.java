1878c2602f965a423e07c0d3934b1dbe6d7b8ee1
Tomasz Bekas <tomasz.bekas@payu.pl>
2014-06-25 13:34:13 +0200

    Removal of @todo tag from global runner

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true, tags = {"@todo"})
+@CucumberOptions(monochrome = true)
 public class RunCukesTest {
 
 }
