d63fb974c43848e2cd4c86304dd02fe0aa3f5536
Sebastian Laskawiec <sebastian.laskawiec@payu.pl>
2014-06-25 14:16:20 +0200

    WIP

--- src/test/java/promocjaProcentowa/Steps.java
+++ src/test/java/promocjaProcentowa/Steps.java
@@ -15,7 +15,7 @@ public class Steps {
     private Produkt produkt;
 
     @Mając("^produkt \"(.*?)\" o cenie ([\\.\\d]+) zł$")
-    public void produkt_o_cenie_zł(/*Masło*/String nazwa, BigDecimal cena) {
+    public void produkt_o_cenie_zł(String nazwa, BigDecimal cena) {
         produkt = PromotionsDSL.utworzNowyProdukt(nazwa, cena);
     }
 
@@ -31,7 +31,6 @@ public class Steps {
     @Wtedy("^w dniu \"(.*?)\" cena produktu \"(.*?)\" wynosi ([\\.\\d]+) zł$")
     public void w_dniu_cena_produktu_wynosi_zł(
             @Format("dd.MM.yyyy") Date dzien, String nazwaProduktu, BigDecimal zalozonaCena) {
-
         BigDecimal cenaProduktu = produkt.dajCene(dzien);
         assertThat(cenaProduktu).isEqualTo(zalozonaCena);
     }
@@ -39,15 +38,15 @@ public class Steps {
 //    @Mając("^dziś jest \"(.*?)\"$")
 //    public void dziś_jest(@Format("dd.MM.yyyy") Date dzien) {
 //    }
-
-    @Kiedy("^ustawiam dla niego promocję na (\\d+)% z długością promocji (\\d+) dni$")
-    public void ustawiam_dla_niego_promocję_na_z_długością_promocji_dni(int procentPromocji, int iloscDni) {
-    }
-
-    @Wtedy("^w \"(.*?)\" dniu cena produktu \"(.*?)\" ([\\.\\d]+) zł$")
-    public void w_dniu_cena_produktu_zł(@Format("dd.MM.yyyy") Date dzien, String produkt,
-                                        BigDecimal cena) {
-    }
+//
+//    @Kiedy("^ustawiam dla niego promocję na (\\d+)% z długością promocji (\\d+) dni$")
+//    public void ustawiam_dla_niego_promocję_na_z_długością_promocji_dni(int procentPromocji, int iloscDni) {
+//    }
+//
+//    @Wtedy("^w \"(.*?)\" dniu cena produktu \"(.*?)\" ([\\.\\d]+) zł$")
+//    public void w_dniu_cena_produktu_zł(@Format("dd.MM.yyyy") Date dzien, String produkt,
+//                                        BigDecimal cena) {
+//    }
 
 
 }
